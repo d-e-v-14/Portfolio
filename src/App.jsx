43494c09fd3c2abb655f15eecca74b0cb41e81d8
d-e-v-14 <mehandrudev@gmail.com>
2025-06-17 23:50:59 +0530

    started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ReactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-w-full min-h-screen bg-purple-950 text-white">
+
+    </div>
+  );
+}
+
+export default App;
