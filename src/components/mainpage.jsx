1c90253073d4f79f7e5c6a2489b4d0dfc8254a05
d-e-v-14 <mehandrudev@gmail.com>
2025-07-06 01:05:59 +0530

    added the star feature

--- src/components/mainpage.jsx
+++ src/components/mainpage.jsx
@@ -11,261 +11,93 @@ import symbol from "../assets/symbol.png";
 import insta from "../assets/insta.png";
 import linkdin from "../assets/linkdin.png";
 import github from "../assets/github.png";
+import Particles from '../utilities/Particles';
+
 
 function About() {
   return (
-    <div className="flex justify-center items-center min-w-full h-[435vh] bg-[#120a20] text-white">
-    <img src={Tiny} 
-    style={{
-        width:'115px',
-        height:'170px',
-        position:'absolute',
-        top:'270px',
-        left:'240px'
-    }} className="z-20"
-    />
+    <div className="relative w-full h-[439vh] bg-[#120a20] text-white overflow-hidden">
+      <div className="fixed inset-0 z-10">
+        <Particles
+          particleColors={['#ffffff', '#ffffff']}
+          particleCount={400}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={120}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
 
-    <img src={Arrow} 
-    style={{
-        width:'105px',
-        height:'120px',
-        position:'absolute',
-        top:'170px',
-        left:'340px'
-    }}
-    />
+      <div style={{ width: '100%', height: '2200px', position: 'relative' }}>
+        <img src={Tiny} style={{ width: '115px', height: '170px', position: 'absolute', top: '270px', left: '240px' }} className="z-20" />
+        <img src={Arrow} style={{ width: '105px', height: '120px', position: 'absolute', top: '170px', left: '340px' }} />
+        <img src={Ellipse} style={{ width: '235px', height: '230px', position: 'absolute', top: '250px', left: '185px' }} className="z-10" />
+        <div style={{ position: 'absolute', top: '234px', left: '160px', width: '270px', height: '270px', borderRadius: '100%', background: 'radial-gradient(circle, rgba(162, 0, 255, 0.9) 0%, rgba(64, 0, 128, 0.4) 60%, rgba(19, 1, 44, 0) 100%)', filter: 'blur(17px)' }} className="z-0" />
 
-    <img src={Ellipse} 
-    style={{
-        width:'235px',
-        height:'230px',
-        position:'absolute',
-        top:'250px',
-        left:'185px'
-    }} className="z-10"
-    />
+        <h3 className="absolute top-48 left-110 font-preahvihear z-20">Hello! I Am Dev Mehandru</h3>
+        <h4 className="absolute top-68 left-104 font-preahvihear text-sm z-20">A Designer who</h4>
+        <p className="absolute top-75 left-106 font-preahvihear text-5xl z-20">
+          Judges a book <br />
+          by its <span className="text-purple-800">cover</span>...
+        </p>
+        <p className="absolute top-102 left-106 font-preahvihear text-[0.65rem] z-20">
+          Because if the cover does not impress you what else can?
+        </p>
 
-    <div
-    style={{
-    position:'absolute',
-    top:'234px',
-    left:'160px',
-    width: '270px',
-    height: '270px',
-    borderRadius: '100%',
-     background: 'radial-gradient(circle, rgba(162, 0, 255, 0.9) 0%, rgba(64, 0, 128, 0.4) 60%, rgba(19, 1, 44, 0) 100%)',
-    filter: 'blur(17px)', 
-   
-    }} className="z-0">
+        <img src={Ellipse_2} style={{ width: '170px', height: '50px', position: 'absolute', top: '350px', left: '560px' }} className="z-10" />
+        <h1 className="absolute text-4xl top-150 left-65 font-preahvihear z-20">I'm a Computer Science Student.</h1>
+        <p className="absolute font-thin top-175 left-65 font-preahvihear z-20">
+          Currently, I'm a Student at VIT Vellore<br /><br /><br />
+          Hi, I'm Dev Mehandru — a passionate and curious Computer Science <br />and Engineering student with a strong interest in web development,<br /> competitive programming, and machine learning. I love combining<br /> creativity with problem-solving to build meaningful digital solutions,<br /> whether it's developing responsive web apps, designing intelligent<br /> systems, or tackling algorithmic challenges. Welcome to my portfolio<br /> — a space where ideas turn into impactful projects.
+        </p>
+
+        <h1 className="absolute text-3xl top-260 left-65 font-preahvihear z-20">Work Experience</h1>
+        <div className="z-0 absolute" style={{ top: '1090px', left: '440px', width: '375px', height: '480px', borderRadius: '100%', background: 'radial-gradient(50% 50% at 50% 50%, #763CAC 0%, rgba(50, 15, 133, 0) 100%)', filter: 'blur(17px)' }}></div>
+
+        <div style={{ background: `linear-gradient(to right, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)` }} className="absolute top-290 left-55 h-35 w-100 flex items-center gap-6 p-6 rounded-xl shadow-lg text-white z-0">
+          <p className="absolute"></p>
+        </div>
+
+        <div style={{ background: `linear-gradient(to right, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)` }} className="absolute top-290 left-160 h-35 w-100 flex items-center gap-6 p-6 rounded-xl shadow-lg text-white z-0"></div>
+        <div style={{ background: `linear-gradient(to right, #130428 7%, #251043 34%, #38126D 57%, #261045 85%, #190634 100%)` }} className="absolute top-330 left-105 h-35 w-100 flex items-center gap-6 p-6 rounded-xl shadow-lg text-white z-0"></div>
+
+        <h1 className="absolute top-295 left-85 text-2xl">Junior Core</h1>
+        <p className="absolute top-305 left-85 text-[0.65rem]">Contributing to technical events, coding sessions,<br /> and collaborative tech projects.</p>
+
+        <h1 className="absolute top-295 left-190 text-2xl">Junior Core</h1>
+        <p className="absolute top-305 left-190 text-[0.65rem]">Responsible for organizing events, coordinating <br />logistics, and ensuring smooth execution of club <br />activities.</p>
+
+        <h1 className="absolute top-337 left-137 text-2xl">Intern</h1>
+        <p className="absolute top-347 left-137 text-[0.65rem]">I assisted in analyzing datasets, generating <br />insights, and supporting data-driven decision <br />making.</p>
+
+        <img src={ieee} style={{ width: '90px', height: '90px', position: 'absolute', top: '1190px', left: '243px' }} className="z-20" />
+        <img src={matrix} style={{ width: '90px', height: '85px', position: 'absolute', top: '1190px', left: '660px' }} className="z-20" />
+        <img src={rsystem} style={{ width: '70px', height: '70px', position: 'absolute', top: '1355px', left: '450px' }} className="z-20" />
+        <img src={base} style={{ width: '100px', height: '100px', position: 'absolute', top: '1345px', left: '440px' }} className="z-10" />
+
+        <img src={solarsys} style={{ width: '1150px', height: '1000px', position: 'absolute', top: '1605px', left: '40px' }} className="z-10" />
+        <p className="absolute top-680 left-60">
+          <span className="text-xl">Contact</span><br /><br /><br />
+          I'm currently looking to join a cross-functional team that values improving<br /> people's lives through accessible design. or have a project in mind? Let's connect.<br /><br />
+          mehandrudev@gmail.com<br />
+          +91 98712 71489
+        </p>
+
+        <img src={symbol} style={{ width: '50px', height: '30px', position: 'absolute', top: '2715px', left: '350px' }} className="z-10" />
+        <a href="https://www.instagram.com/alrde.evz" target="_blank" rel="noopener noreferrer">
+          <img src={insta} style={{ width: '30px', height: '30px', position: 'absolute', top: '2925px', left: '240px' }} className="z-10" />
+        </a>
+        <a href="https://www.linkedin.com/in/dev-mehandru-a4b15b320/?originalSubdomain=in" target="_blank" rel="noopener noreferrer">
+          <img src={linkdin} style={{ width: '30px', height: '30px', position: 'absolute', top: '2925px', left: '290px' }} className="z-10" />
+        </a>
+        <a href="https://github.com/d-e-v-14" target="_blank" rel="noopener noreferrer">
+          <img src={github} style={{ width: '30px', height: '30px', position: 'absolute', top: '2925px', left: '340px' }} className="z-10" />
+        </a>
+      </div>
     </div>
-
-    <h3 className="absolute top-48 left-110 font-preahvihear z-20">
-    Hello! I Am Dev Mehandru
-    </h3>
-
-    <h4 className="absolute top-68 left-104 font-preahvihear text-sm z-20">
-        A Designer who
-    </h4>
-
-    <p className="absolute top-75 left-106 font-preahvihear text-5xl z-20">
-        Judges a book <br />
-        by its <span className="text-purple-800">cover</span>...
-    </p>
-
-    <p className="absolute top-102 left-106 font-preahvihear text-[0.65rem] z-20">
-        Because if the cover does not impress you what else can?
-    </p>
-
-    <img src={Ellipse_2} 
-    style={{
-        width:'170px',
-        height:'50px',
-        position:'absolute',
-        top:'350px',
-        left:'560px'
-    }} className="z-10"
-    />
-    <h1 className="absolute text-4xl top-150 left-65 font-preahvihear z-20">
-        I'm a Computer Science Student. 
-    </h1>
-
-    <p className="absolute font-thin top-175 left-65 font-preahvihear z-20">
-        Currently, I'm a Student at VIT Vellore<br />
-        <br /><br />
-        Hi, I'm Dev Mehandru — a passionate and curious Computer Science <br />and Engineering student with a strong interest in web development,<br /> competitive programming, and machine learning. I love combining<br /> creativity with problem-solving to build meaningful digital solutions,<br /> whether it's developing responsive web apps, designing intelligent<br /> systems, or tackling algorithmic challenges. Welcome to my portfolio<br /> — a space where ideas turn into impactful projects.
-    </p>
-
-    <h1 className="absolute text-3xl top-260 left-65 font-preahvihear z-20">
-        Work Experience
-    </h1>
-
-<div
-  className="z-0 absolute"
-  style={{
-    top: '1090px',
-    left: '440px',
-    width: '375px',
-    height: '480px',
-    borderRadius: '100%',
-    background: 'radial-gradient(50% 50% at 50% 50%, #763CAC 0%, rgba(50, 15, 133, 0) 100%)',
-    filter: 'blur(17px)',
-  }}
-></div>
-
-
-
-    <div style={{background: `linear-gradient(to right,
-      #130428 7%,
-      #251043 34%,
-      #38126D 57%,
-      #261045 85%,
-      #190634 100%)`,
-  }} className="absolute top-290 left-55 h-35 w-100 flex items-center gap-6 p-6 rounded-xl shadow-lg text-white z-0">
-    <p className="absolute">
-
-    </p>
-    </div>
-
-    <div style={{
-    background: `linear-gradient(
-      to right,
-      #130428 7%,
-      #251043 34%,
-      #38126D 57%,
-      #261045 85%,
-      #190634 100%
-    )`,}}
-    className="absolute top-290 left-160 h-35 w-100 flex items-center gap-6 p-6 rounded-xl shadow-lg text-white z-0">
-
-    </div>
-
-    <div style={{
-    background: `linear-gradient(to right,
-      #130428 7%,
-      #251043 34%,
-      #38126D 57%,
-      #261045 85%,
-      #190634 100%)`,
-  }} className="absolute top-330 left-105 h-35 w-100 flex items-center gap-6 p-6 rounded-xl shadow-lg text-white z-0">
-
-    </div>
-
-    <h1 className="absolute top-295 left-85 text-2xl">
-      Junior Core
-    </h1>
-    <p className="absolute top-305 left-85 text-[0.65rem]">
-Contributing to technical events, coding sessions,<br /> and collaborative tech projects.
-    </p>
-
-    <h1 className="absolute top-295 left-190 text-2xl">
-      Junior Core
-    </h1>
-    <p className="absolute top-305 left-190 text-[0.65rem]">
-Responsible for organizing events, coordinating <br />logistics, and ensuring smooth execution of club <br />activities.
-    </p>
-
-    <h1 className="absolute top-337 left-137 text-2xl">
-      Intern
-    </h1>
-    <p className="absolute top-347 left-137 text-[0.65rem]">
-I assisted in analyzing datasets, generating <br />insights, and supporting data-driven decision <br />making.
-    </p>
-
-  <img src={ieee}
-  style={{
-        width:'90px',
-        height:'90px',
-        position:'absolute',
-        top:'1190px',
-        left:'243px'}}      className="z-20"
-  />
-
-
-   <img src={matrix}
-  style={{
-        width:'90px',
-        height:'85px',
-        position:'absolute',
-        top:'1190px',
-        left:'660px'}}      className="z-20"
-  />
-
-
-     <img src={rsystem}
-  style={{
-        width:'70px',
-        height:'70px',
-        position:'absolute',
-        top:'1355px',
-        left:'450px'}}      className="z-20"
-  />
-     <img src={base}
-  style={{
-        width:'100px',
-        height:'100px',
-        position:'absolute',
-        top:'1345px',
-        left:'440px'}}      className="z-10"
-  />
-<img src={solarsys} 
-    style={{
-        width:'1150px',
-        height:'1000px',
-        position:'absolute',
-        top:'1605px',
-        left:'40px'}}
-        className="z-10"
-/>
-  <p className="absolute top-680 left-60">
-    <span className="text-xl">Contact</span>
-    <br />
-  <br />
-  <br />
-    I'm currently looking to join a cross-functional team that values improving<br /> people's lives
-  through accessible design. or have a project in mind? Let's connect.
-  <br /><br />
-  mehandrudev@gmail.com<br />
-  +91 98712 71489
-  </p>
-  <img src={symbol} 
-  style={{
-        width:'50px',
-        height:'30px',
-        position:'absolute',
-        top:'2715px',
-        left:'350px'}}
-        className="z-10"/>
-<a href="https://www.instagram.com/alrde.evz" target="_blank" rel="noopener noreferrer">
-  <img src={insta} 
-  style={{
-        width:'30px',
-        height:'30px',
-        position:'absolute',
-        top:'2925px',
-        left:'240px'}}
-        className="z-10"/></a>
-<a href="https://www.linkedin.com/in/dev-mehandru-a4b15b320/?originalSubdomain=in" target="_blank" rel="noopener noreferrer">
-  <img src={linkdin} 
-  style={{
-        width:'30px',
-        height:'30px',
-        position:'absolute',
-        top:'2925px',
-        left:'290px'}}
-        className="z-10"/></a>
-<a href="https://github.com/d-e-v-14" target="_blank" rel="noopener noreferrer">
-  <img src={github} 
-  style={{
-        width:'30px',
-        height:'30px',
-        position:'absolute',
-        top:'2925px',
-        left:'340px'}}
-        className="z-10"/> </a>
-    </div> 
   );
 }
 
 export default About;
-
-
